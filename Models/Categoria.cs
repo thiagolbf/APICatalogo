0c322a2ef5f2380655a282795706aae380114e23
Thiago Barcelos <thiagolbf@hotmail.com>
2025-04-28 20:26:28 -0300

    apply data annotations for entities and migrations to add data into database

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,26 @@
-﻿namespace APICatalogo.Models;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+
+namespace APICatalogo.Models;
+
+[Table("Categorias")]
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    [Key]
     public int CategoriaId { get; set; }
-    public string? Nome { get; set; }    
+
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
     public ICollection<Produto>? Produtos { get; set; }
