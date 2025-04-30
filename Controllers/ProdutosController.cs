d610a8c08d502f9d50488390fe62e85f75a2a2be
Thiago Barcelos <thiagolbf@hotmail.com>
2025-04-30 20:57:59 -0300

    add produtos controller and method get

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,40 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            if (produtos is null) return NotFound("Produtos não encontrados...");
+
+
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto == null) return NotFound($"Produto {id} não encontrado ");
+
+            return produto;
+        }
+    }
+}
